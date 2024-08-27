b6ea75f933be5407c52a35dbed4ad278e2db742c
stu.chen <best_css@foxmail.com>
2024-08-27 17:41:14 +0800

    version:  0.0.5

--- dist/main.d.ts
+++ dist/main.d.ts
@@ -10,6 +10,7 @@ interface Options {
  * @param {string} options.folderName - 相对于src的路径，默认为'services'
  * @param {string} options.className - 导出类名，默认为'Api'
  * @param {string} options.mode - 导出文件类型，默认为'ts' 'ts' | 'js'
+ * @param {boolean} options.log - 是否打印日志，默认为true
  * @returns
  */
 declare function VitePluginApiGenerator(options?: Options): {
